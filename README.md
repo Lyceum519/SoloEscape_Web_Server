c8ed6c966e7372ad5893d61d87d51eb9222e360f
YangpaSok <wsw9131@naver.com>
2017-11-19 02:34:28 +0900

    mysql module 추가

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # SoloEscape_Web_Server
-Node.js web server for SoloEscape
+SoloEscape Server using Express
+
+directory structure 
+
+(예시)
+SoloEscape_Web_Server/
+├── package.json
+├── bin
+│   └──
+├── public
+│   └── images
+│   └── javascripts
+│   └── stylesheets
+├── routes
+│   └── index.js
+│   └── users.js
+├── app.js
+└── views
+    └── error.jade
+    └── index.jade
+    └── layout.jade
+
--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//for mysqlTest
+var mysqlTest = require('./routes/mysqlTest');
+
 var app = express();
 
 // view engine setup
@@ -24,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/mysqlTest', mysqlTest);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
