ebdfe2e61333205e2639b0a52e4ffa6056a17a3a
YangpaSok <wsw9131@naver.com>
2017-12-06 21:31:58 +0900

    googleapis dependency 추가, client 코드 연동

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var apiReqInfo = require('./routes/apiReqTest');
 
 //for mysqlTest
-var mysqlTest = require('./routes/mysqlTest');
+// var mysqlTest = require('./routes/mysqlTest');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../SoloEscape_Web_Client/build'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,11 +25,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/mysqlTest', mysqlTest);
+
+app.use(express.static(path.join(__dirname, '../SoloEscape_Web_Client/build')));
+
+
+
+app.use('/', index);  //api 외의 모든 요청
+
+app.use('/api', apiReqInfo);  //api 관련 요청
+
 
 
 // catch 404 and forward to error handler
