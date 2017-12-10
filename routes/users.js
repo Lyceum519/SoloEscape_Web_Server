d849e54927514e77f5802d570a61ade97c3179a8
sangwu <wsw9131@naver.com>
2017-12-10 17:16:44 +0900

    /apiTest?val=abc  요청시 , string 리턴

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: req.param("val") });
+  // res.render('index', { title: req.param("val") });
+    res.send("req param val value: " + req.param("val"));
 });
 
 module.exports = router;
