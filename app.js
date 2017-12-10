3b294444cda0c7b8ee676cc073fe69e1578ee61e
sangwu <wsw9131@naver.com>
2017-12-10 15:46:18 +0900

    google oauth 인증 관련 작업

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
+// 외부 모듈 추출
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
+// 사용자 정의 모듈 추출
 var index = require('./routes/index');
-var apiReqInfo = require('./routes/apiReqTest');
+var users = require('./routes/users');
 
-//for mysqlTest
-// var mysqlTest = require('./routes/mysqlTest');
+var oauth2Callback = require('./routes/oauth2Callback'); // 인증 관련 test
 
+// 서버 생성
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, '../SoloEscape_Web_Client/build'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// 서버 설정 - view engine setup
+// 일반 test 시
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// react build folder 이용시 ( 파일 위치 고려하기 )
+// app.set('views', path.join(__dirname, '../SoloEscape_Web_Client/build'));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 
 
+// 미들웨어 설정
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// 일반 test 시
+app.use(express.static(path.join(__dirname, 'public')));
+// react build folder 이용시 ( 파일 위치 고려하기 )
+// app.use(express.static(path.join(__dirname, '../SoloEscape_Web_Client/build')));
 
 
-app.use(express.static(path.join(__dirname, '../SoloEscape_Web_Client/build')));
+// 라우터 미들웨어 설정
+app.use('/', index);
+app.use('/users', users);
+
+app.use('/oauth2callback', oauth2Callback); // 인증 관련 callback test
 
 
 
-app.use('/', index);  //api 외의 모든 요청
-
-app.use('/api', apiReqInfo);  //api 관련 요청
-
-
+// 에러 핸들러
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,6 +55,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -55,4 +68,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// 모듈화
 module.exports = app;
